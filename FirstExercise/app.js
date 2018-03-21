9969c31c1475f4362c792868960ad5a3cd6ee294
Unknown <lingtrevor@gmail.com>
2018-03-21 16:09:28 +0200

    Complete console app!
    Take names from input and add them to the names array. Find matching names from story string and replace these with names that starts with uppercase letter.

--- FirstExercise/app.js
+++ FirstExercise/app.js
@@ -2,48 +2,41 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    terminal: false
 });
 
 var names = [];
 var recursiveAsyncReadLine = () => {
-    console.log('* Väljumiseks sisestage: exit')
-    rl.question('* Sisetage nimesid ühe kaupa: \n', (name) => {
+    rl.question(' * Uus nimi: \n', (name) => {
         //we need some base case, for recursion
         if (name == 'exit') {
-            console.log('Väljusite nimede sisestamisest- list sisestatud nimedest -> \n')
-            console.log('Array: ', names);
-            names.forEach((name) => {
-                console.log('ForEach: ', name);
-            });
+            console.log('Väljusite nimede sisestamisest- list sisestatud nimedest -> \n ->' + names, '\n');
+            var story = "The data liia was gathered using Facebook's peeter infrastructure at that time, and many helen other developers had taken advantage of it - but the data was not authorised for piia them to share with others. The other key point is that even the people directly taking part in the margus personality quiz would have had no idea that they were potentially sharing their data oskar with Donald Trump's election campaign. ";
+            var splitStory = story.split(' ');
+            for (var i = 0; i < splitStory.length; i++) {
+                for (var j = 0; j < names.length; j++) {
+                    if (splitStory[i].toLowerCase() === names[j].toLowerCase()) {
+                        splitStory[i] = names[j];
+                    };
+                };
+            };
+            var splitStory = splitStory.join(' ');
+            console.log(splitStory);
+
             //closing RL and returning from function.
             return rl.close();
-        }
+        };
         var correctName = name.charAt(0).toUpperCase() + name.slice(1);
         names.push(correctName);
-        console.log('Got it! Name was: "', name, '"\n');
 
-        //Calling this function again to ask new question
+        //Calling this function again to ask new name 
         recursiveAsyncReadLine();
     });
 };
-
+console.log(' * Väljumiseks sisestage: exit \n * Sisestage nimed ühe kaupa!');
 recursiveAsyncReadLine();
 
-
-var story = "The data liia was gathered using Facebook's peeter infrastructure at that time, and many helen other developers had taken advantage of it - but the data was not authorised for piia them to share with others. The other key point is that even the people directly taking part in the margus personality quiz would have had no idea that they were potentially sharing their data oskar with Donald Trump's election campaign. ";
-var splitStory = story.split(' ');
-var name = ['Peeter', 'Helen', 'Piia', 'Margus', 'Oskar', 'Liia'];
-
-for(var i = 0; i < splitStory.length; i++){
-    for(var j = 0; j < name.length; j++){
-        if(splitStory[i].toLowerCase()===name[j].toLowerCase()){
-            splitStory[i] = name[j];
-
-        }
-    }
-}
-console.log(splitStory);
 
 
 
