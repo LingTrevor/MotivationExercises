9999a528e2e91a7d065e1cc45db9bbedae6b7784
Unknown <lingtrevor@gmail.com>
2018-03-21 12:38:25 +0200

    Working solution with hardcoded data
    Lines: 34-46

--- FirstExercise/app.js
+++ FirstExercise/app.js
@@ -30,19 +30,11 @@ var recursiveAsyncReadLine = () => {
 
 recursiveAsyncReadLine();
 
-//////////////////
-//Testimiseks ->//
-//////////////////
 
 var story = "The data liia was gathered using Facebook's peeter infrastructure at that time, and many helen other developers had taken advantage of it - but the data was not authorised for piia them to share with others. The other key point is that even the people directly taking part in the margus personality quiz would have had no idea that they were potentially sharing their data oskar with Donald Trump's election campaign. ";
 var splitStory = story.split(' ');
-//console.log(splitStory);
 var name = ['Peeter', 'Helen', 'Piia', 'Margus', 'Oskar', 'Liia'];
-//console.log(name);
-//name = name.map(name => name.toLowerCase());
-//console.log(name);
 
-var correctNames = [];
 for(var i = 0; i < splitStory.length; i++){
     for(var j = 0; j < name.length; j++){
         if(splitStory[i].toLowerCase()===name[j].toLowerCase()){
@@ -52,23 +44,7 @@ for(var i = 0; i < splitStory.length; i++){
     }
 }
 console.log(splitStory);
-// correctNames = correctNames.map(correctNames=> correctNames.charAt(0).toUpperCase()+ correctNames.slice(1));
-// console.log(correctNames);
 
-// for( var i = 0; i < splitStory.length; i++){
-//     if(splitStory[i] === 'data'){
-//         splitStory[i] = 'tere';
-//         console.log('Works');
-//     }
-//    //console.log(splitStory[i]);
-// };
-// console.log(splitStory);
-
-
-
-// for(var j = 0; j < name.length; j++){
-//     console.log(name[j]);
-// };
 
 
 
